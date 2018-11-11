c75fbeb2f8d0fbdfdb0916120f167fe83b8e8cdb
YueJZJM <yuejzhfut@163.com>
2018-11-12 00:32:45 +0800

    第七章

---
+++ CustomAnim/app/src/main/java/com/example/customanim/CustomTV.java
@@ -0,0 +1,42 @@
+package com.example.customanim;
+
+import android.graphics.Camera;
+import android.graphics.Matrix;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+
+public class CustomTV extends Animation {
+
+    private int mCenterWidth;
+    private int mCenterHeight;
+    private Camera mCamera = new Camera();
+    private float mRotateY = 0.0f;
+
+    @Override
+    public void initialize(int width, int height, int parentWidth, int parentHeight) {
+        super.initialize(width, height, parentWidth, parentHeight);
+        //设置时长
+        setDuration(1000);
+        // 动画结束后保留状态
+        setFillAfter(true);
+        // 设置默认插值器
+        setInterpolator(new AccelerateInterpolator());
+        mCenterHeight = height / 2;
+        mCenterWidth = width / 2;
+    }
+
+    public void setmRotateY(float rorateY) {
+        mRotateY = rorateY;
+    }
+
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t) {
+        super.applyTransformation(interpolatedTime, t);
+        final Matrix matrix = t.getMatrix();
+        matrix.preScale(1,
+                1 - interpolatedTime,
+                mCenterWidth,
+                mCenterHeight);
+    }
+}
